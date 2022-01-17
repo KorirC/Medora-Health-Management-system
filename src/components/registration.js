b582f6741091894a44f61cc920f9323f09f10765
Cynthia Chebet <chebetcynthia8@gmail.com>
2022-01-17 10:13:32 +0300

    setting up the development environment

---
+++ src/components/registration.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Form, TextInput } from "carbon-components-react";
+
+const PatientRegistration = () => {
+  return (
+    <div>
+      <Form>
+        <TextInput></TextInput>
+      </Form>
+    </div>
+  );
+};
+export default PatientRegistration;
