d3159e14c4e37d8516f0f8ffaa344d33f631c5b9
Cynthia Chebet <chebetcynthia8@gmail.com>
2022-01-17 10:38:38 +0300

    created a patient registration form

--- src/components/registration.js
+++ src/components/registration.js
@@ -1,12 +1,62 @@
 import React from "react";
-import { Form, TextInput } from "carbon-components-react";
+import {
+  Grid,
+  Row,
+  Column,
+  Form,
+  TextInput,
+  Select,
+  SelectItem,
+} from "carbon-components-react";
+import styles from "./registration.scss";
 
 const PatientRegistration = () => {
   return (
     <div>
-      <Form>
-        <TextInput></TextInput>
-      </Form>
+      <Grid narrow className={styles.grid} style={{ marginTop: "10%" }}>
+        <Row>
+          <Column lg={3}></Column>
+          <Column lg={6}>
+            <Form>
+              <TextInput
+                name="name"
+                id="name"
+                labelText="Name"
+                placeholder="Enter Name"
+                // onChange={}
+              ></TextInput>
+              <br />
+              <TextInput
+                name="age"
+                id="age"
+                labelText="Age"
+                placeholder="Enter Age"
+              ></TextInput>
+              <br />
+              <Select>
+                {" "}
+                <SelectItem value="female" text="Female" />
+                <SelectItem value="male" text="Male" />
+              </Select>
+              <br />
+              <TextInput
+                name="idnumber"
+                id="idnumber"
+                labelText="ID Number"
+                placeholder="Enter National ID Number"
+              ></TextInput>
+              <br />
+              <TextInput
+                name="phoneNumber"
+                id="phoneNumber"
+                labelText="Phone Number"
+                placeholder="Enter Phone Number"
+              ></TextInput>
+            </Form>
+          </Column>
+          <Column lg={3}></Column>
+        </Row>
+      </Grid>
     </div>
   );
 };
