3e31211badea3af80421f578a56bfd9e64691a8a
Cynthia Chebet <chebetcynthia8@gmail.com>
2022-01-17 11:09:57 +0300

    created a function to handle data on submission

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { Button } from "carbon-components-react";
 import PatientRegistration from "./components/registration";
 
 function App() {
   return (
     <div className="App">
       <PatientRegistration />
-      <Button>create a react app</Button>
     </div>
   );
 }
--- src/components/registration.js
+++ src/components/registration.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Grid,
   Row,
@@ -7,23 +7,43 @@ import {
   TextInput,
   Select,
   SelectItem,
+  Button,
 } from "carbon-components-react";
 import styles from "./registration.scss";
 
 const PatientRegistration = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [id, setId] = useState("");
+  const [phonenumber, setPhoneNumber] = useState("");
+  //   console.log(name, age, gender, id, phonenumber);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let data = JSON.stringify({
+      name: `${name}`,
+      gender: `${gender}`,
+      age: `${age}`,
+      nationalID: `${id}`,
+      phonenumber: `${phonenumber}`,
+    });
+    console.log(data);
+  };
+
   return (
     <div>
       <Grid narrow className={styles.grid} style={{ marginTop: "10%" }}>
         <Row>
           <Column lg={3}></Column>
           <Column lg={6}>
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <TextInput
                 name="name"
                 id="name"
                 labelText="Name"
                 placeholder="Enter Name"
-                // onChange={}
+                onChange={(e) => setName(e.target.value)}
               ></TextInput>
               <br />
               <TextInput
@@ -31,9 +51,10 @@ const PatientRegistration = () => {
                 id="age"
                 labelText="Age"
                 placeholder="Enter Age"
+                onChange={(e) => setAge(e.target.value)}
               ></TextInput>
               <br />
-              <Select>
+              <Select onChange={(e) => setGender(e.target.value)}>
                 {" "}
                 <SelectItem value="female" text="Female" />
                 <SelectItem value="male" text="Male" />
@@ -44,6 +65,7 @@ const PatientRegistration = () => {
                 id="idnumber"
                 labelText="ID Number"
                 placeholder="Enter National ID Number"
+                onChange={(e) => setId(e.target.value)}
               ></TextInput>
               <br />
               <TextInput
@@ -51,7 +73,12 @@ const PatientRegistration = () => {
                 id="phoneNumber"
                 labelText="Phone Number"
                 placeholder="Enter Phone Number"
+                onChange={(e) => setPhoneNumber(e.target.value)}
               ></TextInput>
+              <br />
+              <Button kind="secondary" type="submit">
+                Save
+              </Button>
             </Form>
           </Column>
           <Column lg={3}></Column>
